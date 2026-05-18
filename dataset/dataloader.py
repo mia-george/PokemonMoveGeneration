9271f733ff7ac117423b9341166fa13a2e3e1830
mia-george <68865403+mia-george@users.noreply.github.com>
2026-05-17 22:24:02 -0500

    added more data processing

--- dataset/dataloader.py
+++ dataset/dataloader.py
@@ -1,6 +1,19 @@
 import pandas as pd
+import roman
 
-pokemon_df = pd.read_csv("data/metadata_pokemon.csv")
-moves_df = pd.read_csv("data/metadata_pokemon_moves.csv")
+moves_df = pd.read_csv("data/metadata_pokemon_moves.csv", encoding="utf-8")
 
-merged_df = moves_df.merge(pokemon_df, on="id")
+# Data cleaning
+
+# shorten generation column to just number and convert from roman numeral
+moves_df['generation'] = moves_df['generation'].apply(lambda x: x.split(" ")[-1])
+moves_df['generation'] = moves_df['generation'].apply(lambda x: roman.fromRoman(str(x)))
+
+# fix column name 
+moves_df = moves_df.rename(columns={'short_descripton': 'short_description'})
+
+# clean short description column to remove $effect_chance%
+moves_df['short_description'] = moves_df['short_description'].str.replace('$effect_chance% ', '', regex=False)
+
+print(moves_df.head(20))
+
