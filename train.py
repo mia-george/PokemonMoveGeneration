ecadf30f88cb5c227d71b1b7721d9951b1369ff2
mia-george <68865403+mia-george@users.noreply.github.com>
2026-05-18 15:25:12 -0500

    created training file

---
+++ train.py
@@ -0,0 +1,8 @@
+from datasets.dataloader import get_dataloaders
+from transformers import T5ForConditionalGeneration, T5Tokenizer
+
+tokenizer = T5Tokenizer.from_pretrained("t5-small")
+model = T5ForConditionalGeneration.from_pretrained("t5-small")
+
+train_loader, val_loader, test_loader = get_dataloaders(tokenizer)
+
