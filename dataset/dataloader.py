20db92839348ad0d08437c068003284102ebcfd4
mia-george <68865403+mia-george@users.noreply.github.com>
2026-06-06 16:09:44 -0500

    updated data cleaning

--- dataset/dataloader.py
+++ dataset/dataloader.py
@@ -4,12 +4,9 @@ from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset, DataLoader
 
 moves_df = pd.read_csv("data/metadata_pokemon_moves.csv", encoding="utf-8")
+augmented_df = pd.read_csv("data/augmented_moves.csv", encoding="utf-8")
 
 # Data cleaning
-
-# shorten generation column to just number and convert from roman numeral
-moves_df['generation'] = moves_df['generation'].apply(lambda x: x.split(" ")[-1])
-moves_df['generation'] = moves_df['generation'].apply(lambda x: roman.fromRoman(str(x)))
 
 # fix column name 
 moves_df = moves_df.rename(columns={'short_descripton': 'short_description'})
@@ -17,14 +14,15 @@ moves_df = moves_df.rename(columns={'short_descripton': 'short_description'})
 # clean short description column to remove $effect_chance%
 moves_df['short_description'] = moves_df['short_description'].str.replace('$effect_chance% ', '', regex=False)
 
-# fix empty values in power and accuracy columns
-moves_df["power"] = moves_df["power"].fillna("none")
-moves_df["accuracy"] = moves_df["accuracy"].fillna("none")
+# only need these columns
+moves_df = moves_df[["name", "short_description", "type", "power", "accuracy"]]
 
-# remove duplicate descriptions so each description maps to exactly one move
-desc_counts = moves_df.groupby("short_description")["name"].count()
-unique_descs = desc_counts[desc_counts == 1].index
-moves_df = moves_df[moves_df["short_description"].isin(unique_descs)]
+# Then combine
+combined_df = pd.concat([moves_df, augmented_df], ignore_index=True)
+
+# fix empty values in power and accuracy columns
+combined_df["power"] = combined_df["power"].fillna("none")
+combined_df["accuracy"] = combined_df["accuracy"].fillna("none")
 
 # print(moves_df.head(20))
 
